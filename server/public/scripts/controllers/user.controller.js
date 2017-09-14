5069defd80f03deb658e559e43660228c89e7276
Adam Biessener <abiessener@gmail.com>
2017-09-14 14:38:40 -0500

    fix logout button

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -2,6 +2,7 @@ myApp.controller('UserController', function(UserService) {
   console.log('UserController created');
   var self = this;
   self.userObject = UserService.userObject;
+  self.userService = UserService;
 
   self.editCurrentUser = (name, avatar_url) => {
     UserService.editCurrentUser(name, avatar_url);
