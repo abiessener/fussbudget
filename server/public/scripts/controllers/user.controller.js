9bae65275b94b1ac4798b0affe887656423eed2e
Adam Biessener <abiessener@gmail.com>
2017-09-14 13:56:38 -0500

    user edit functionality

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -1,6 +1,9 @@
 myApp.controller('UserController', function(UserService) {
   console.log('UserController created');
-  var vm = this;
-  vm.userService = UserService;
-  vm.userObject = UserService.userObject;
+  var self = this;
+  self.userObject = UserService.userObject;
+
+  self.editCurrentUser = (name, avatar_url) => {
+    UserService.editCurrentUser(name, avatar_url);
+  }
 });
