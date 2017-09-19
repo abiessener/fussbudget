09aaf29d2d6ef990d89929aef9b438395e937f35
Adam Biessener <abiessener@gmail.com>
2017-09-19 16:01:36 -0500

    added edit-wakeup files that were missed in previous commits

---
+++ server/public/scripts/controllers/edit-wakeup.controller.js
@@ -0,0 +1,38 @@
+myApp.controller('EditWakeupController', function (ScheduleService, ClientService, $location, $mdDialog) {
+  console.log('EditWakeupController created');
+
+  var self = this;
+
+  self.loadedSchedule = ScheduleService.loadedSchedule;
+
+  // find the existing wakeup time
+  for (var i = 0; i < self.loadedSchedule.list.length; i++) {
+    var element = self.loadedSchedule.list[i];
+
+    if (element.name == 'wakeup') {
+      self.wakeUpTime = element.time;
+      break;
+    }
+  }
+
+  self.wakeUpTime = new Date(self.wakeUpTime);
+  self.wakeUpTime.setTime(self.wakeUpTime.getTime() + self.wakeUpTime.getTimezoneOffset() * 60 * 1000);
+
+  self.setWakeUpNow = () => {
+    let now = new Date();
+    console.log('now', now);
+    
+    now = new Date(1970,0,1,now.getHours(), now.getMinutes());
+    console.log('now', now);
+    
+    now.setTime(now.getTime() - now.getTimezoneOffset() * 60 * 1000);
+    
+    $mdDialog.hide(now);
+  }
+
+  self.editWakeUp = () => {
+    self.wakeUpTime.setTime(self.wakeUpTime.getTime() - self.wakeUpTime.getTimezoneOffset() * 60 * 1000);
+    $mdDialog.hide(self.wakeUpTime);
+  }
+
+});
