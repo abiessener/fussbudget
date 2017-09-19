859b549b3ec7820f70285d3f12e4ce8f8e5c5cad
Adam Biessener <abiessener@gmail.com>
2017-09-19 16:14:33 -0500

    client router authentication wrapping

--- server/routes/client.router.js
+++ server/routes/client.router.js
@@ -11,117 +11,126 @@ var DefaultSchedules = require('../models/defaultschedules.schema.js');
 // handles creating new clients
 router.post('/', (req, res) => {
   // console.log('/client POST hit', req.body);
+  if (req.isAuthenticated()) {
 
-  req.body.primary_caregiver = req.user._id;
 
-  // we have the user input in req.body, but we need to get our default schedule and put it on there or the schema will reject it
-  let defaultSchedule = [];
-  DefaultSchedules.findOne({
-    'name': req.body.age
-  }, (err, data) => {
-    if (err) {
-      console.log('/client find error:', err);
-      res.sendStatus(500);
-      return;
-    } else {
-      //happy path
-      // console.log('------------\ndata:', data);
+    req.body.primary_caregiver = req.user._id;
 
-      req.body.schedule_template = data.events;
-      // console.log('req.body.schedule_template:', req.body.schedule_template);
+    // we have the user input in req.body, but we need to get our default schedule and put it on there or the schema will reject it
+    let defaultSchedule = [];
+    DefaultSchedules.findOne({
+      'name': req.body.age
+    }, (err, data) => {
+      if (err) {
+        console.log('/client find error:', err);
+        res.sendStatus(500);
+        return;
+      } else {
+        //happy path
+        // console.log('------------\ndata:', data);
 
-      let clientToSave = new Client(req.body);      
+        req.body.schedule_template = data.events;
+        // console.log('req.body.schedule_template:', req.body.schedule_template);
 
-      clientToSave.save((err, data) => {
-        if (err) {
-          console.log('/client POST save error:', err);
-          res.sendStatus(500);
-        } else {
-          //happy path
-          res.sendStatus(201);
-        }
-      });
-    }
-  });
+        let clientToSave = new Client(req.body);
+
+        clientToSave.save((err, data) => {
+          if (err) {
+            console.log('/client POST save error:', err);
+            res.sendStatus(500);
+          } else {
+            //happy path
+            res.sendStatus(201);
+          }
+        });
+      }
+    });
+  }
 })
 
 // returns a list of clients associated with a user
 router.get('/list', (req, res) => {
   // console.log('\n------------------\n/list GET hit');
+  if (req.isAuthenticated()) {
 
-  Client.find({
-    primary_caregiver: req.user.id
-  }, (err, data) => {
-    if (err) {
-      console.log('/client find error:', err);
-      res.sendStatus(500);
-      return;
-    } else {
-      //happy path      
-      res.send(data);
-    }
-  }); // end find
+    Client.find({
+      primary_caregiver: req.user.id
+    }, (err, data) => {
+      if (err) {
+        console.log('/client find error:', err);
+        res.sendStatus(500);
+        return;
+      } else {
+        //happy path      
+        res.send(data);
+      }
+    }); // end find
+  }
 })
 
 // returns a client with the id passed in the URL parameter
 router.get('/:id', (req, res) => {
   // console.log('\n------------------\n/client GET hit with id:', req.params.id);
+  if (req.isAuthenticated()) {
 
-  Client.find({
-    _id: req.params.id
-  }, (err, data) => {
-    if (err) {
-      console.log('/client find error:', err);
-      res.sendStatus(500);
-      return;
-    } else {
-      //happy path
-      res.send(data);
-    }
-  }); // end find
-})
+    Client.find({
+      _id: req.params.id
+    }, (err, data) => {
+      if (err) {
+        console.log('/client find error:', err);
+        res.sendStatus(500);
+        return;
+      } else {
+        //happy path
+        res.send(data);
+      }
+    }); // end find
+  }
+});
 
 // updates a Client entry
 router.put('/', (req, res) => {
   // console.log('\n------------------\n/client PUT hit with client:', req.body);
+  if (req.isAuthenticated()) {
 
-  let clientToUpdate = req.body;
+    let clientToUpdate = req.body;
 
-  Client.findByIdAndUpdate({
-    _id: clientToUpdate._id
-  }, {
-    $set: {
-      name: clientToUpdate.name,
-      date_of_birth: clientToUpdate.date_of_birth,
-      notes: clientToUpdate.notes,
-      avatar_url: clientToUpdate.avatar_url
-    }
-  }, (err, data) => {
-    if (err) {
-      console.log('findByIdAndUpdate error:', err);
-      res.sendStatus(500);
-    } else {
-      res.sendStatus(200);
-    }
-  }); // end findByIdAndUpdate
-
+    Client.findByIdAndUpdate({
+      _id: clientToUpdate._id
+    }, {
+      $set: {
+        name: clientToUpdate.name,
+        date_of_birth: clientToUpdate.date_of_birth,
+        notes: clientToUpdate.notes,
+        avatar_url: clientToUpdate.avatar_url
+      }
+    }, (err, data) => {
+      if (err) {
+        console.log('findByIdAndUpdate error:', err);
+        res.sendStatus(500);
+      } else {
+        res.sendStatus(200);
+      }
+    }); // end findByIdAndUpdate
+  }
 });
 
 // deletes a Client from the db
 router.delete('/:id', (req, res) => {
   // console.log('\n------------------\n/client DELETE hit with id:', req.params.id);
+  if (req.isAuthenticated()) {
 
-  Client.findByIdAndRemove({
-    _id: req.params.id
-  }, (err, data) => {
-    if (err){
-      console.log('findByIdAndRemove error:', err);
-      res.sendStatus(500);
-    } else {
-      res.sendStatus(200);
-    }
-  });
-
+    Client.findByIdAndRemove({
+      _id: req.params.id
+    }, (err, data) => {
+      if (err) {
+        console.log('findByIdAndRemove error:', err);
+        res.sendStatus(500);
+      } else {
+        res.sendStatus(200);
+      }
+    });
+  }
 }) // end DELETE route
 
 module.exports = router;
