4354a9cc178471f7e3e4586f6a4168b6528d53d6
Adam Biessener <abiessener@gmail.com>
2017-09-20 10:40:56 -0500

    sidenav v1

--- server/public/scripts/controllers/nav.controller.js
+++ server/public/scripts/controllers/nav.controller.js
@@ -1,4 +1,4 @@
-myApp.controller('NavController', ['UserService', function(UserService){
+myApp.controller('NavController', ['UserService', '$mdSidenav', '$location', function(UserService, $mdSidenav, $location){
   // console.log('NavController');
 
   var self = this;
@@ -6,4 +6,16 @@ myApp.controller('NavController', ['UserService', function(UserService){
   // all we need currently this for is to display the user's avatar in the nav
   self.userObject = UserService.userObject;
   
+  self.closeNav = () => {
+    $mdSidenav('right').close();
+  }
+
+  self.openNav = () => {
+    $mdSidenav('right').toggle();
+  }
+
+  self.go = (url) => {
+    $location.path(url);
+    $mdSidenav('right').close();
+  }
 }]);
