bd97b5f0ea8119077141217b127da4d5ab5342e9
Adam Biessener <abiessener@gmail.com>
2017-09-22 08:33:26 -0500

    fixed double form submit on mobile

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,8 @@
 var myApp = angular.module('myApp', ['ngRoute', 'ngMaterial']);
 
 /// Routes ///
-myApp.config(function($routeProvider, $locationProvider, $mdThemingProvider) {
+myApp.config(function($routeProvider, $locationProvider, $mdThemingProvider, $mdGestureProvider) {
+  $mdGestureProvider.skipClickHijack();
   $locationProvider.hashPrefix('');
   $mdThemingProvider.theme('default')
     .primaryPalette('deep-purple')
